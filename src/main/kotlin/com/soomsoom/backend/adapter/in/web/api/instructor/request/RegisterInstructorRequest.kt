4ec44bc6aa0bbeabc86e0b5cba7f462ec64b91c7
sanghoon <tkdgns5817@gmail.com>
2025-07-29 02:18:12 +0900

    feat(#22): instructor 생성 시 사용하는 dto 생성
    1. request
    2. response dto 팩토리 메서드 패턴 적용

--- src/main/kotlin/com/soomsoom/backend/adapter/in/web/api/instructor/request/RegisterInstructorRequest.kt
+++ src/main/kotlin/com/soomsoom/backend/adapter/in/web/api/instructor/request/RegisterInstructorRequest.kt
@@ -1,15 +1,16 @@
 package com.soomsoom.backend.adapter.`in`.web.api.instructor.request
 
+import com.soomsoom.backend.application.port.`in`.common.FileMetadata
 import com.soomsoom.backend.application.port.`in`.instructor.command.RegisterInstructorCommand
 
 data class RegisterInstructorRequest(
     val name: String,
-    val profileImageUrl: String?,
+    val profileImageMetadata: FileMetadata?,
     val bio: String?,
 )
 
 fun RegisterInstructorRequest.toCommand() = RegisterInstructorCommand(
     name,
-    profileImageUrl,
+    profileImageMetadata,
     bio
 )
--- src/main/kotlin/com/soomsoom/backend/application/port/in/instructor/dto/RegisterInstructorResult.kt
+++ src/main/kotlin/com/soomsoom/backend/application/port/in/instructor/dto/RegisterInstructorResult.kt
@@ -1,7 +1,27 @@
 package com.soomsoom.backend.application.port.`in`.instructor.dto
 
+import com.soomsoom.backend.common.exception.SoomSoomException
+import com.soomsoom.backend.domain.instructor.InstructorErrorCode
+import com.soomsoom.backend.domain.instructor.model.Instructor
+
 data class RegisterInstructorResult(
     val instructorId: Long,
     val preSignedUrl: String,
     val fileKey: String,
-)
+) {
+    companion object {
+        fun from(
+            instructor: Instructor,
+            preSignedUrl: String,
+            fileKey: String,
+        ): RegisterInstructorResult {
+            return RegisterInstructorResult(
+                instructorId = instructor.id ?: throw SoomSoomException(
+                    InstructorErrorCode.INSTRUCTOR_ID_CANNOT_BE_NULL
+                ),
+                preSignedUrl = preSignedUrl,
+                fileKey = fileKey
+            )
+        }
+    }
+}
