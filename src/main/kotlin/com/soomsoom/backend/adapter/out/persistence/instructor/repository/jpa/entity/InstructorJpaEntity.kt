a3aed0b739f1d692c9a9989e5348ec6f886db489
sanghoon <tkdgns5817@gmail.com>
2025-07-29 21:44:58 +0900

    feat(#22): instructor jpa entity 수정
    1. 팩토리 메서드 수정
    2. update 메서드 추가

--- src/main/kotlin/com/soomsoom/backend/adapter/out/persistence/instructor/repository/jpa/entity/InstructorJpaEntity.kt
+++ src/main/kotlin/com/soomsoom/backend/adapter/out/persistence/instructor/repository/jpa/entity/InstructorJpaEntity.kt
@@ -2,6 +2,7 @@ package com.soomsoom.backend.adapter.out.persistence.instructor.repository.jpa.e
 
 import com.soomsoom.backend.common.entity.BaseTimeEntity
 import com.soomsoom.backend.domain.instructor.model.Instructor
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -14,19 +15,36 @@ class InstructorJpaEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
+
+    @Column(nullable = false)
     var name: String,
-    var profileImageUrl: String?,
+
+    @Column(columnDefinition = "TEXT")
     var bio: String?,
+
+    var profileImageUrl: String?,
+
 ) : BaseTimeEntity() {
 
     // 도메인 객체를 DB 엔티티로 변환
     companion object {
         fun from(instructor: Instructor): InstructorJpaEntity {
-            return InstructorJpaEntity(instructor.id ?: 0L, instructor.name, instructor.profileImageUrl, instructor.bio)
+            return InstructorJpaEntity(
+                instructor.id ?: 0L,
+                instructor.name,
+                instructor.profileImageUrl,
+                instructor.bio
+            )
         }
     }
 
     fun toDomain(): Instructor {
         return Instructor(this.id, this.name, this.profileImageUrl, this.bio)
+    }
+
+    fun update(instructor: Instructor) {
+        this.name = instructor.name
+        this.bio = instructor.bio
+        this.profileImageUrl = instructor.profileImageUrl
     }
 }
