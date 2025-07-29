dcb5d8f64c46f635fca2f06700f6162194ade0d8
sanghoon <tkdgns5817@gmail.com>
2025-07-29 23:01:36 +0900

    feat(#22): register service tc 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:5.6.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.mockk:mockk:1.13.10")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.springframework.security:spring-security-test")
 }
