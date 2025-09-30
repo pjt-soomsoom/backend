c9a485fe85eb1fd00a37dbc5724f03bc1a042a2a
sanghoon <tkdgns5817@gmail.com>
2025-09-30 18:47:55 +0900

    chore: cicd

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,8 +5,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.36"
     }
+  }
+  backend "s3" {
+      bucket         = "soomsoom-terraform-state-bucket" # 1단계에서 만든 S3 버킷 이름
+      key            = "global/terraform.tfstate"      # S3 버킷 내에 상태 파일이 저장될 경로
+      region         = "ap-northeast-2"
+      dynamodb_table = "soomsoom-terraform-state-lock"   # 1단계에서 만든 DynamoDB 테이블 이름
+      encrypt        = true
+      profile        = "soomsoom"
   }
 }
 
