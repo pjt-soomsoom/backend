61200b9e6d5a1c8ebf7717f483c84f23775d9b5e
sanghoon <tkdgns5817@gmail.com>
2025-10-01 19:05:40 +0900

    chore: ami 변경

--- terraform/modules/app/asg_ec2.tf
+++ terraform/modules/app/asg_ec2.tf
@@ -1,8 +1,6 @@
 resource "aws_launch_template" "main" {
     name_prefix   = "${var.project_name}-${var.environment}-"
-    # arm64 아키텍처용 최신 Amazon Linux 2023 AMI (서울 리전 기준)
-    image_id      = "ami-02cf627e019f25aeb"
-    # test와 prod 환경 모두 t4g.small 인스턴스 타입을 사용하도록 통일합니다.
+    image_id      = "ami-024bf8a1313cb6b6"
     instance_type = "t4g.small"
 
     vpc_security_group_ids = [aws_security_group.app.id]
